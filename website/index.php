b94c103388df35a4fad5a5ebe97b8635154dd20d
Marco Lebbink <marco.lebbink@sanoma.com>
2019-02-03 00:10:11 +0100

    Refresh 5min

--- website/index.php
+++ website/index.php
@@ -207,7 +207,7 @@
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<script>
-		setInterval(function() { window.location.reload(true); }, 900000);  // refresh page every 15 minutes
+		setInterval(function() { window.location.reload(true); }, 300000);  // refresh page every 15 minutes
 	</script>
 </body>
 </html>
